100239b6c8b75a08322c2e925a3e57a2de0f7ee8
Joachim Bargsten <jw@bargsten.org>
2021-09-29 14:11:50 +0200

    fixed rollup issues

--- rollup.config.js
+++ rollup.config.js
@@ -6,11 +6,13 @@ import pkg from "./package.json"
 export default [
   {
     input: "src/index.ts",
-    output: [{ file: pkg.main, format: "cjs" }],
+    external: ["lodash/toString", "lodash/truncate"],
+    output: [{ file: pkg.main, format: "cjs", exports: "default" }],
     plugins: [commonjs(), typescript()],
   },
   {
     input: "src/index.ts",
+    external: ["lodash/toString", "lodash/truncate"],
     output: [{ file: pkg.module, format: "es" }],
     plugins: [typescript()],
   },
