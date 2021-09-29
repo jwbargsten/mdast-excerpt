0778509d09914299a25c2eb5468d432f86746008
Joachim Bargsten <jw@bargsten.org>
2021-09-29 10:16:51 +0200

    start with fixing rollup and pkg.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
   root: true,
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
   extends: [
     'eslint:recommended',
-    'prettier',
     'plugin:jest/recommended',
     'plugin:prettier/recommended',
   ],
-    "rules": {
+  "rules": {
     'prettier/prettier': 'error',
-    }
+  }
 };
